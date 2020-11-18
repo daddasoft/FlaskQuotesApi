3481e789ba1459fcebe9238590e3d4519b4f434b
daddasoft <studio.dadda@gmail.com>
2020-11-19 00:41:53 +0100

    create a pagination Helper

---
+++ utils/PaginationCreator.py
@@ -0,0 +1,11 @@
+from math import ceil
+
+
+def PgCreator(QuoteCount, currentPage, perPage):
+    currentPage = int(currentPage)
+    pageCount = ceil(float(QuoteCount / perPage))
+    nextPage = currentPage+1 if currentPage + 1 <= pageCount else None
+    prevPage = currentPage-1 if currentPage - 1 > 0 else None
+
+    return {"pagination": {"nextpage": nextPage,
+                           "currentPage": currentPage, "prevPage": prevPage, "pages": pageCount}}
