b3d74267f126339284622faf3474c9e12f56465f
daddasoft <studio.dadda@gmail.com>
2020-11-18 23:07:20 +0100

    move logic to Controllers

--- server.py
+++ server.py
@@ -1,34 +1,26 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, escape
 from config.env import env
 from controllers import QuoteController as Quote
-from models.Quote import create as QuoteCreate
+
+
 QuotesApp = Flask(__name__)
 QuotesApp.config['SECRET_KEY'] = env("appSecret")
 
 
 @QuotesApp.route("/")
-# @deco
 def index():
     return "Hello"
 
 
 @QuotesApp.route("/quotes", methods=["POST"])
 def create():
-    data = request.get_json()
-    body = data["body"]
-    category = data["category"]
-    createdBy = data["createdBy"]
-    author = data["author"]
-    createQuote = QuoteCreate(body, author, createdBy, category)
-    if(createQuote["status"]):
-        return jsonify(createQuote["data"]), 201
-    return jsonify(createQuote["message"]), 400
+    return Quote.store()
 
 
 @QuotesApp.route("/quotes",)
 def getQuotes():
     data = Quote.get()
-    if("message" not in data):
+    if("message" not in data and len(data) != 0):
         return jsonify(data), 200
     return jsonify(data), 404
 
