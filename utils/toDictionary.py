c1ddd19f10bb6ae7454d77d4cd7413afc5cc7440
daddasoft <studio.dadda@gmail.com>
2020-11-19 00:00:38 +0100

    remove CreatedBy Field from returning

--- utils/toDictionary.py
+++ utils/toDictionary.py
@@ -5,5 +5,5 @@ def ToDictionary(oldObj):
     data = []
     for obj in oldObj:
         data.append({"id": escape(obj[0]), "body": escape(obj[1]), "author": escape(obj[2]),
-                     "createdBy": escape(obj[3]), "category": escape(obj[4]), "createdAt": escape(obj[5]), "updatedAt": escape(obj[6])})
+                     "category": escape(obj[4]), "createdAt": escape(obj[5]), "updatedAt": escape(obj[6])})
     return data
