d72d8769aaba76f21b3d1ead86d895c18839a78c
daddasoft <studio.dadda@gmail.com>
2020-11-18 22:19:22 +0100

    add more validation roles

--- middlewares/Quoteinfo.py
+++ middlewares/Quoteinfo.py
@@ -4,12 +4,12 @@ from flask import request, jsonify
 def SetDefault(fun):
     def nestedFunc():
         data = request.get_json()
-        if("body" not in data):
+        if("body" not in data or len(data["body"]) < 5):
             return jsonify({"message": "Body Should Contain At Least 5 Characters",
                             "field": "body"}), 400
-        if("category" not in data):
+        if("category" not in data or len(data["category"]) < 2):
             data["category"] = "other"
-        if("author" not in data):
+        if("author" not in data or len(data["author"]) < 2):
             data["author"] = "Unknown"
         return fun()
     return nestedFunc
