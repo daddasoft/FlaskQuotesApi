ed3433b93b5e02cd8a15deac2775dff963e11c88
daddasoft <studio.dadda@gmail.com>
2020-11-18 22:51:12 +0100

    add escape for HTML Tags

--- utils/toDictionary.py
+++ utils/toDictionary.py
@@ -1,6 +1,9 @@
+from flask import escape
+
+
 def ToDictionary(oldObj):
     data = []
     for obj in oldObj:
-        data.append({"id": obj[0], "body": obj[1], "author": obj[2],
-                     "createdBy": obj[3], "category": obj[4], "createdAt": obj[5], "updatedAt": obj[6]})
+        data.append({"id": escape(obj[0]), "body": escape(obj[1]), "author": escape(obj[2]),
+                     "createdBy": escape(obj[3]), "category": escape(obj[4]), "createdAt": escape(obj[5]), "updatedAt": escape(obj[6])})
     return data
