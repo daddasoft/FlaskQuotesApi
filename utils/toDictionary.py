3a890491dcab34fd43311bc22e950274a3e7fb3d
daddasoft <studio.dadda@gmail.com>
2020-11-18 21:39:27 +0100

    Fix spelling error

--- utils/toDictionary.py
+++ utils/toDictionary.py
@@ -2,5 +2,5 @@ def ToDictionary(oldObj):
     data = []
     for obj in oldObj:
         data.append({"id": obj[0], "body": obj[1], "author": obj[2],
-                     "createdBy": obj[3], "categorie": obj[4], "createdAt": obj[5], "updatedAt": obj[6]})
+                     "createdBy": obj[3], "category": obj[4], "createdAt": obj[5], "updatedAt": obj[6]})
     return data
