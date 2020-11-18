80d5f0feba560b9f4d5d6bb229a832a46d2431f4
daddasoft <studio.dadda@gmail.com>
2020-11-18 22:50:27 +0100

    optimize Code

--- middlewares/checkValideJsonFormat.py
+++ middlewares/checkValideJsonFormat.py
@@ -5,7 +5,7 @@ from flask.json import jsonify
 def CheckJson(fun):
     def nestedFunc():
         try:
-            data = request.get_json()
+            request.get_json()
         except:
             return jsonify({"message": "Make Sure You Have a valid Json Format"}), 400
         return fun()
