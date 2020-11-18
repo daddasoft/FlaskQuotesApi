a5f73e973c23224914e86ff881e06f9b1e868a45
daddasoft <studio.dadda@gmail.com>
2020-11-18 20:31:53 +0100

    Initialize The project

---
+++ config/database.py
@@ -0,0 +1,15 @@
+from config.env import env
+import mysql.connector
+
+
+def connect():
+    try:
+        database = mysql.connector.connect(
+            host=env("host"),
+            user=env("user"),
+            passwd=env("pass"),
+            database=env("database")
+        )
+        return database
+    except:
+        return None
