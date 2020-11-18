f59950ada0c860ee53e915c10a6527e0fff1d5b3
daddasoft <studio.dadda@gmail.com>
2020-11-19 00:54:01 +0100

    fix a bug (prev page when the current bigger that the total)

--- utils/PaginationCreator.py
+++ utils/PaginationCreator.py
@@ -5,7 +5,8 @@ def PgCreator(QuoteCount, currentPage, perPage):
     currentPage = int(currentPage)
     pageCount = ceil(float(QuoteCount / perPage))
     nextPage = currentPage+1 if currentPage + 1 <= pageCount else None
-    prevPage = currentPage-1 if currentPage - 1 > 0 else None
+    prevPage = currentPage-1 if currentPage - \
+        1 > 0 and currentPage - 1 < pageCount else None
 
     return {"pagination": {"nextpage": nextPage,
                            "currentPage": currentPage, "prevPage": prevPage, "pages": pageCount}}
