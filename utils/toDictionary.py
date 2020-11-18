07fca5c57203cbd2b8bb529810ca46329cbcd8db
daddasoft <studio.dadda@gmail.com>
2020-11-18 20:45:06 +0100

    Add a Utility Function

---
+++ utils/toDictionary.py
@@ -0,0 +1,7 @@
+def ToDictionary(oldObj):
+    data = []
+    for obj in oldObj:
+        data.append({"id": obj[0], "body": obj[1], "author": obj[2],
+                     "createdBy": obj[3], "categorie": obj[4], "createdAt": obj[5], "updatedAt": obj[6]})
+        print(data)
+    return data
