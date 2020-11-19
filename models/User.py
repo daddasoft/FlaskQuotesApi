b3539a4ef31b8a263db9985f7dc89ca2682308e8
daddasoft <studio.dadda@gmail.com>
2020-11-19 12:50:30 +0100

    Move password Hashing to The Controller 🥂

--- models/User.py
+++ models/User.py
@@ -1,11 +1,9 @@
 from config.database import connect
-from werkzeug.security import generate_password_hash
 
 
 def store(username, email, password, role="user"):
     try:
         database = connect()
-        password = generate_password_hash(password)
         cursor = database.cursor()
         sql = "INSERT INTO users (username, email, password,role, created_at, updated_at) \
             VALUES (%s,%s,%s,%s,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP())"
