f746b2ac43bfc6a4c2420250e5623477246b1ade
daddasoft <studio.dadda@gmail.com>
2020-11-19 14:09:52 +0100

    add User Register Validator

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,11 +1,17 @@
 from flask import request, render_template
+from flask.helpers import url_for
 from werkzeug.security import generate_password_hash
+from werkzeug.utils import redirect
+from models.User import store
+
+from middlewares.UserRegister import RegisterValidator
 
 
+@RegisterValidator
 def create():
     username = request.form["username"]
     email = request.form["email"]
     password = generate_password_hash(request.form["password"])
-    confirmPassword = request.form["password-confirm"]
-    print(username, email, password, confirmPassword)
+    if(store(username, email, password)):
+        return redirect(url_for('index'))
     return render_template('auth/register.html')
