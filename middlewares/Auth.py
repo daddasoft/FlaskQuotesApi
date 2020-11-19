569ddae8d2d40046fa408740163ed0040b8a96fd
daddasoft <studio.dadda@gmail.com>
2020-11-19 23:38:27 +0100

    add auth check for deleting 🧨

---
+++ middlewares/Auth.py
@@ -0,0 +1,12 @@
+from flask import session
+from flask.helpers import url_for
+from flask.json import jsonify
+from werkzeug.utils import redirect
+
+
+def isAuth(fun):
+    def nestedFunc(*args):
+        if("user" not in session):
+            return jsonify({"message": "You are not logged in to perform this action"}), 401
+        return fun()
+    return nestedFunc
