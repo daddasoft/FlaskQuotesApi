d6086976efd4fadbdb39e5bbbf0d17680d81824b
daddasoft <studio.dadda@gmail.com>
2020-11-20 22:57:49 +0100

    fix args in middleware

--- middlewares/Auth.py
+++ middlewares/Auth.py
@@ -8,5 +8,5 @@ def isAuth(fun):
     def nestedFunc(*args):
         if("user" not in session):
             return jsonify({"message": "You are not logged in to perform this action"}), 401
-        return fun()
+        return fun(*args)
     return nestedFunc
