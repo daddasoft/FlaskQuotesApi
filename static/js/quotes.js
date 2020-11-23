ade333d4f1a2d291f78705ad5846b2052394870a
daddasoft <studio.dadda@gmail.com>
2020-11-23 13:26:48 +0100

    fix js

--- static/js/quotes.js
+++ static/js/quotes.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("quoteForm");
-const API_URL = "http://localhost:5000/api/quotes";
+const API_URL = "/api/quotes";
 const button = document.getElementById("btn");
 const cardContainer = document.querySelector(".cardContainer");
 if (form) {
