be2f002326c9316b668574d0bf1b6fc9f1ec4be5
daddasoft <studio.dadda@gmail.com>
2020-11-27 17:15:49 +0100

    Delete old env res

--- config/env.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path  # Python 3.6+ only
-from dotenv import load_dotenv
-import os
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
-
-def env(key):
-    return os.getenv(key)
