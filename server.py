a6d2b94a85eb15aa3da7ffad6ff3c98622f0e942
daddasoft <studio.dadda@gmail.com>
2020-12-02 17:24:56 +0100

    add User Api Routes

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify, render_template, session, redirect, url_for
+from flask import Flask, jsonify, request, render_template, session, redirect, url_for
 from flask_cors import CORS
-from config.env import env
+from werkzeug.security import check_password_hash
+from env import env
 from controllers import QuoteController as Quote
 from controllers import UserController as User
+from utils.session_checker import session_check
+from models.User import getUser
+import jwt
+from datetime import timedelta, datetime
 QuotesApp = Flask(__name__)
-QuotesApp.config['SECRET_KEY'] = env("appSecret")
+QuotesApp.config['SECRET_KEY'] = "o#PNGQAGdD8NjmCi"
 CORS(QuotesApp)
 
 
@@ -13,12 +18,12 @@ def index():
     return Quote.homeIndex()
 
 
-@QuotesApp.route("/api/quotes", methods=["POST"])
+@QuotesApp.route("/quotes", methods=["POST"])
 def create():
     return Quote.store()
 
 
-@QuotesApp.route("/api/quotes/<id>", methods=["DELETE"])
+@QuotesApp.route("/quotes/<id>", methods=["DELETE"])
 def delete(id):
     return Quote.delete(id)
 
@@ -41,6 +46,8 @@ def apiDelete(id):
     if(Quote.Quote.destroy(id, userID) == 1):
         return jsonify([]), 200
     return jsonify({"error": "can't Delete this Quote"})
+
+
 @QuotesApp.route("/api/quotes", methods=["POST"])
 def apiStore():
     if("authorization" not in request.headers):
@@ -67,6 +74,29 @@ def apiStore():
     if(res["status"]):
         return jsonify(res["data"][0]), 200
     return jsonify(res), 400
+
+
+@QuotesApp.route("/api/login", methods=["POST"])
+def getToken():
+    return User.api_login(None)
+
+
+@QuotesApp.route("/change-password", methods=["GET"])
+def change_password():
+    if(not "user" in session):
+        return redirect(url_for("login"))
+
+    return render_template('auth/change-password.html', title="quotes | change password")
+
+
+@QuotesApp.route("/change-password", methods=["POST"])
+def change_password_post():
+    if(not "user" in session):
+        return redirect(url_for("login"))
+    return User.change_password()
+
+
+@QuotesApp.route("/api/quotes/",)
 def getQuotes():
     data = Quote.index()
     if("message" not in data and len(data) != 0):
@@ -76,14 +106,43 @@ def getQuotes():
 
 @QuotesApp.route("/register")
 def register():
-    if("user" in session):
-        return redirect(url_for('index'))
+    session_check()
     return render_template('auth/register.html', title="Register")
 
 
 @QuotesApp.route("/register", methods=["POST"])
 def registerPost():
     return User.create()
+
+
+@QuotesApp.route("/api/register", methods=["POST"])
+def registerAPI():
+    return User.api_register()
+
+
+@QuotesApp.route("/archive", methods=["GET"])
+def archive():
+    if(not "user" in session):
+        return redirect(url_for("login"))
+    res = Quote.get_archive()
+    return render_template('archive.html', data=res, title="quotes | my quotes")
+
+
+@QuotesApp.route("/api/me", methods=["GET"])
+def checkAuth():
+    if("authorization" not in request.headers):
+        return jsonify({"msg": "Unauthorized"}), 401
+    token = str(request.headers["authorization"]).strip().split("Bearer ")[-1]
+    try:
+        decoded = jwt.decode(
+            token, key=env("JWT_SECRET"), algorithms="HS256")
+        userID = decoded["userId"]
+    except:
+        return jsonify({"msg": "Invalid / Expaired Token"}), 401
+    user = getUser(userID)
+    if(not user["status"]):
+        return jsonify({"msg": "User can't be found"}), 404
+    return user
 
 
 @QuotesApp.route("/login", methods=["POST"])
@@ -93,8 +152,7 @@ def loginPost():
 
 @QuotesApp.route("/login")
 def login():
-    if("user" in session):
-        return redirect(url_for('index'))
+    session_check()
     return render_template('auth/login.html', title="Login")
 
 
@@ -108,5 +166,10 @@ def logout():
     return User.logout()
 
 
+@QuotesApp.errorhandler(404)
+def error(errorCode):
+    return jsonify({"error": "Resources Not Found"}), 404
+
+
 if(__name__ == "__main__"):
-    QuotesApp.run()
+    QuotesApp.run(debug=True)
