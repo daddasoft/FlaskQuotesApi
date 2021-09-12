760cfcc413941da4245a3154c776a1be8cef4e5d
daddasoft <studio.dadda@gmail.com>
2021-09-12 23:04:31 +0100

    fix type cast

--- utils/PaginationCreator.py
+++ utils/PaginationCreator.py
@@ -1,12 +1,13 @@
 from math import ceil
+from env import env
 
 
 def PgCreator(QuoteCount, currentPage, perPage):
     currentPage = int(currentPage)
-    pageCount = ceil(float(QuoteCount / perPage))
+    pageCount = ceil(float(QuoteCount / int(perPage)))
     nextPage = currentPage+1 if currentPage + 1 <= pageCount else None
     prevPage = currentPage-1 if currentPage - \
         1 > 0 and currentPage - 1 < pageCount else None
 
     return {"pagination": {"nextpage": nextPage,
-                           "currentPage": currentPage, "prevPage": prevPage, "pages": pageCount}}
+                           "currentPage": currentPage, "prevPage": prevPage, "pages": pageCount, "perPage": int(env("paginate") or 5)}}
